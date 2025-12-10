f765c239b9ac576c5a04008cf5a1cb25454d7503
Oliver Lee <oliverzlee@gmail.com>
2025-12-09 23:06:30 -0600

    add example with different compilation modes
    Change-Id: Ibbf573bdfddb211785a2f7c7a9d372c040f8d201

---
+++ examples/compilation_mode/check_flag.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+workspace_root="$1"
+target="$2"
+pattern="$3"
+
+cd "$workspace_root"
+bazelisk aquery \
+    "mnemonic(CppCompile, deps($target))" \
+    --curses=yes \
+    --output=jsonproto \
+  | jq -r '.actions[].arguments[]' \
+  | grep --word-regexp --  "$pattern"
