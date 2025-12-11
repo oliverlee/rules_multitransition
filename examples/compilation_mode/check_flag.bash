3a7956e208eac30b66e6524dd0920a44569aa87c
Oliver Lee <oliverzlee@gmail.com>
2025-12-10 23:11:44 -0600

    add tests to run examples
    Change-Id: I72c8c5cb2a1e40f68ad400936df088ba6c6117b6

--- examples/compilation_mode/check_flag.bash
+++ examples/compilation_mode/check_flag.bash
@@ -6,9 +6,11 @@ target="$2"
 pattern="$3"
 
 cd "$workspace_root"
+
 bazelisk aquery \
     "mnemonic(CppCompile, deps($target))" \
     --curses=yes \
     --output=jsonproto \
+    "${BAZEL_VENDOR_DIR:+--vendor_dir="$BAZEL_VENDOR_DIR"}" \
   | jq -r '.actions[].arguments[]' \
-  | grep --word-regexp --  "$pattern"
+  | grep --word-regexp -- "$pattern"
